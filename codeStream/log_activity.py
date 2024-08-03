2c3bce4fa9b5a80b62ca5c9c3fdaa63337f0a4fd
Dongchao Shen <afkxwz@gmail.com>
2024-08-03 16:18:43 +0800

    删除不必要的日志文件

--- codeStream/log_activity.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-# Configure logging
-logging.basicConfig(filename='study_log.log', level=logging.INFO, format='%(asctime)s - %(message)s')
-
-
-def log_activity(activity):
-    logging.info(activity)
